ffcdee7f20d43978d0d9e8d82c303319351c6408
Navy <navydotgif@gmail.com>
2020-11-24 00:41:52 +0200

    Identify payload

--- CBot/DiscordPayloads/Identify.cs
+++ CBot/DiscordPayloads/Identify.cs
@@ -19,26 +19,26 @@ namespace CBot.DiscordPayloads
         {
             public string token { get; private set; }
 
-            //public bool compress { get; private set; }
-            //public bool guild_subscriptions { get; private set; }
+            public bool compress { get; private set; }
+            public bool guild_subscriptions { get; private set; }
 
             public int intents { get; private set; }
-            //public int large_threshold { get; private set; }
-            //public int[] shard { get; private set; }
+            public int large_threshold { get; private set; }
+            public int[] shard { get; private set; }
 
-            //public Presence presence { get; private set; }
+            public Presence presence { get; private set; }
             public Dictionary<string, string> properties { get; private set; }
 
             public IdentifyData()
             {
 
-                //this.compress = false;
-                //this.guild_subscriptions = true;
+                this.compress = false;
+                this.guild_subscriptions = true;
 
-                //this.large_threshold = 50;
-                //this.shard = null;
+                this.large_threshold = 50;
+                this.shard = null;
 
-                //this.presence = null;
+                this.presence = null;
                 this.properties = new Dictionary<string, string>();
                 this.properties.Add("$os", $"{Environment.OSVersion}");
                 this.properties.Add("$browser", "C# Lib");
