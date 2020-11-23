d778a3af0e9b056b07506d4229d2e7edd6f8411d
Navy <navydotgif@gmail.com>
2020-11-24 00:52:49 +0200

    Preliminary interfaces

---
+++ CBot/Interfaces/Channel.cs
@@ -0,0 +1,19 @@
+﻿using CBot.RESTOptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBot.Interfaces
+{
+    interface IChannel
+    {
+
+        Task<IChannel> Edit(ChannelEditOptions Options);
+
+        Task<IChannel> Delete();
+
+        Task<IChannel> OverwritePermissions(PermissionOverwriteOptions Options);
+
+    }
+}
---
+++ CBot/Interfaces/TextChannel.cs
@@ -0,0 +1,36 @@
+﻿using CBot.RESTOptions;
+using CBot.Structures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBot.Interfaces
+{
+    interface ITextChannel : IChannel
+    {
+
+        Task<Message> FetchMessage(long Id);
+
+        Task<Dictionary<long, Message>> FetchMessages(MessageQueryOptions Options);
+
+        Task<Message> Send(string Text = null, MessageOptions Options = null);
+
+        Task<Message> EditMessage(long Id, string Text = null, MessageOptions Options = null);
+
+        Task<Message> DeleteMessage(long Id);
+
+        Task<Dictionary<long, Message>> BulkDeleteMessages(long[] Ids);
+
+        Task<Dictionary<long, Message>> BulkDeleteMessages(Dictionary<long, Message> Ids);
+
+        Task<MessageReaction> AddReaction(long MessageId, MessageReaction Reaction);
+
+        Task<MessageReaction> DeleteReaction(long MessageId, MessageReaction Reaction, long Target);
+
+        Task<MessageReaction> DeleteReactions(long MessageId, MessageReaction Reaction = null);
+
+        Task<MessageReaction> FetchReactions(long MessageId, MessageReaction Reaction, ReactionQueryOptions Options = null);
+
+    }
+}
