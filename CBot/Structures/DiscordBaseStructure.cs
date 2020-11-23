3eb7e34925b90b9048c8700964f1b54ae37dbdd1
Navy <navydotgif@gmail.com>
2020-11-24 00:45:33 +0200

    Base structure

---
+++ CBot/Structures/DiscordBaseStructure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+
+namespace CBot.Structures
+{
+    class DiscordBaseStructure
+    {
+        public long Id { get; internal set; }
+        public BaseClient Client { get; internal set; }
+
+        public DiscordBaseStructure(BaseClient Client, JsonElement Id)
+        {
+            this.Id = long.Parse(Id.GetString());
+            this.Client = Client;
+            Console.WriteLine($"Creating DiscordStructure with ID: {this.Id}");
+        }
+
+    }
+}
