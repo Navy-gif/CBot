7770c6fd4308e818bbc8da6be1e51bfddb56a0f9
Navy <navydotgif@gmail.com>
2020-11-24 00:42:09 +0200

    Preliminary structures

---
+++ CBot/Structures/Guild.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace CBot.Structures
+{
+    class Guild : DiscordBaseStructure
+    {
+
+        public Guild(BaseClient Client, Dictionary<string, JsonElement> Data) : base(Client, Data["id"])
+        {
+            Console.WriteLine($"Creating Guild with ID: {this.Id}");
+        }
+
+    }
+}
---
+++ CBot/Structures/Message.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+
+namespace CBot.Structures
+{
+    class Message : DiscordBaseStructure
+    {
+
+        public Message(BaseClient Client, Dictionary<string, JsonElement> Data) : base(Client, Data["id"])
+        {
+            
+        }
+
+    }
+}
