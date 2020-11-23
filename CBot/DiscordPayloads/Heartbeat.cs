bfc7824c180a4755cedb1b71dd405b4a913dfc77
Navy <navydotgif@gmail.com>
2020-11-23 16:10:09 +0200

    preliminary payloads folder

---
+++ CBot/DiscordPayloads/Heartbeat.cs
@@ -0,0 +1,20 @@
+﻿namespace CBot.DiscordPayloads
+{
+    class Payload
+    {
+        public int op { get; set; }
+        public int s { get; set; }
+        public string t { get; set; }
+        public object d { get; set; }
+    }
+    class Heartbeat : Payload
+    {
+
+        public new object s { get; set; } = null;
+        public Heartbeat()
+        {
+            this.op = 1;
+            
+        }
+    }
+}
