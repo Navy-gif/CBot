f73dafe652305f2ecb81984903ee05493e9be89b
Navy <navydotgif@gmail.com>
2020-11-26 01:21:36 +0200

    types

--- CBot/Util/Types.cs
+++ CBot/Util/Types.cs
@@ -7,5 +7,19 @@ namespace CBot.Util
     enum ChannelType
     {
 
+        Text,
+        Dm,
+        Voice,
+        GroupDm,
+        Category,
+        News,
+        Store
+
+    }
+
+    enum OverwriteType
+    {
+        Role,
+        Member
     }
 }
